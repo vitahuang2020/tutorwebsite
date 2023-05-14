58df44cd891b403323b2a9a03cd8f6f72e3fc445
Daisy <daisymou090202@gmail.com>
2023-05-14 15:29:58 -0400

    Changed line 40 exampleRadios2 -> exampleRadios3 Fixed id reference error

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-# hello there
+# hello there hi hi
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
