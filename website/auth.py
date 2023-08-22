1bf176d7c0644b5358c401df5c4ee681b51639da
kitzi666 <130304216+kitzi666@users.noreply.github.com>
2023-08-21 22:43:00 -0400

    making sure my commit works

--- website/auth.py
+++ website/auth.py
@@ -6,7 +6,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
-
+#hello
 @auth.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
